828e7ee15f5fcaa0340bd3a8e84266e19bf8bf7b
TecFelipeDsMartins <tec.felipedsmartins@gmail.com>
2020-12-10 08:06:02 -0300

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import Product from "./Product";
 import "./style.css";
 
+import TelaDeAbertura from './TelaDeAbertura'
+
+
 export default function App() {
   return (
     <>
+      <TelaDeAbertura/>
       <Product
         img={
           "https://blog.santaprata.com.br/wp-content/uploads/2017/09/121880-conheca-as-6-dicas-de-ouro-para-vender-joias-e-semijoias.png"
@@ -13,7 +17,6 @@ export default function App() {
         desc={"Lindo Anel mais Pulseira Banhada a ouro"}
         link={"Anel%20e%20pulseira%20"}
       />
-
       <Product
         img={
           "https://joiasboz.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/r/brincos-da-moda-asas-anjo-dourados-folheados-ouro-semi-joias.png"
---
+++ src/TelaDeAbertura.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const TelaDeAbetura = styled.section`
+  background-color: #fba;
+  height: 100vh;
+  margin: 0;
+`
+
+export default props => {
+  return (
+    
+       <TelaDeAbetura>
+
+       </TelaDeAbetura>
+    
+  )
+};
